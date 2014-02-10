5e9ac8711482b1061e56cb54ed48eff7af9a4159
ctschwope <cschwope@F89R3R1-CSCHWOP.medassets.com>
2014-02-10 17:06:13 -0600

    First pass at files from old project

---
+++ sotm.rb
@@ -0,0 +1,128 @@
+
+
+
+class Action
+  attr_reader :name, :card_type, :action_type, :text, :power_invoke
+  
+  def initialize(name, card_type, action_type, text, power_invoke = [])
+    @name = name
+    @card_type = card_type
+    @action_type = action_type
+    @text = text
+    @power_invoke = power_invoke
+  end
+  
+  def invokable?
+    return power_invoke.length != 0
+  end
+  
+  def to_s
+    @name + ", " + @card_type.to_s + ", " + @action_type.to_s + ", " + text 
+  end
+end
+
+class ActionChain < Array
+  
+  def to_s
+    out_str = ""
+    self.each do | action | 
+      out_str += "  " + action.to_s + "\n"
+    end
+    out_str
+  end
+end
+
+class ActionChainGenerator
+  attr_reader :chains
+  def initialize(action_list, invoke_types = [:Power])
+    @action_list = action_list
+    @chains = []
+    @invoke_types = invoke_types
+  end
+  
+  def to_s
+    return "No Chains Found" if @chains.length == 0
+    out_str = ""
+    @chains.each_with_index do |chain, index| 
+      out_str += "Chain " + (index + 1).to_s + ":\n" + chain.to_s
+    end
+    out_str
+  end
+  
+  def generate_chains
+    @chains = []
+    return if @action_list.length == 0
+    invokable_actions = @action_list.find_all {|x| @invoke_types.include?(x.action_type) }
+    invokable_actions.each do | action | 
+      if action.invokable? 
+        invoke_action(action)
+      else
+        @chains << ActionChain.new([action])
+      end
+    end
+  end
+  
+  def invoke_action(action)
+    next_action_list = @action_list.reject { | x | x == action }
+    next_act_chain = ActionChainGenerator.new(next_action_list, action.power_invoke)
+    next_act_chain.generate_chains
+    if next_act_chain.chains.length == 0 then
+      @chains << ActionChain.new([action])
+    else
+      next_act_chain.chains.each { | x |  @chains << ActionChain.new([action] + x) }
+    end
+  end
+end
+
+
+
+class ActionSet
+  
+  def self.init_action_list
+    @actions = []
+    @actions << Action.new("Rhapsody of Vigor", :Melody, :Perform, "Up to 5 targets regain 1 HP each")
+    @actions << Action.new("The Ardent Adept", :Character, :Power, "Execute Perform text on a card", [:Perform])
+    @actions << Action.new("Syncopated Onslaught", :Rhythm, :Perform, "Select up to 2 targets. until the start of your next turn increase damage death bu those targets by 1")
+    @actions << Action.new("Syncopated Onslaught", :Rhythm, :Accompany, "The ardent adept deals 1 target 1 sonic damage")
+    @actions << Action.new("Sarabande of Destruction", :Melody, :Perform, "Destroy 1 ongoing or environment card")
+  end
+  
+  def to_s
+    @actions.join("\n") 
+  end
+  
+  def self.get_by_name(name)
+    init_action_list if @actions.nil?
+    @actions.find_all {|x| x.name == name }
+  end
+end
+
+def SingleRun
+  
+  def initialize
+    @actionHolder = ActionHolder.new
+    @powers = @actionHolder.actions.find_all {|x| x.action_type = :Power }
+    @usedPowers = []
+
+  end
+
+  def GetRunString
+    
+  end
+end
+
+
+
+# select power
+# trigger power
+# if does another power, select power again, repeat, removing power form possuble lists
+
+#actionHolder =  ActionHolder.new
+#powers = actionHolder.actions.find_all{| x | x.action_type == :Power }
+#print powers.to_s
+#print "\n\n"
+#print actionHolder.to_s + "\n"
+#print "\n\n"
+
+
+
---
+++ sotm_test.rb
@@ -0,0 +1,81 @@
+require 'test/unit'
+require '.\sotm.rb'
+
+class TestAction< Test::Unit::TestCase
+
+  def setup
+    @action = ActionSet.get_by_name("Rhapsody of Vigor")[0]
+  end
+  def test_initialize_not_power
+    assert_equal(:Melody, @action.card_type)
+    assert_equal(:Perform, @action.action_type)
+    assert_equal("Rhapsody of Vigor", @action.name)
+    assert_equal("Up to 5 targets regain 1 HP each", @action.text)
+    assert_equal([], @action.power_invoke)
+    assert(!@action.invokable?)
+  end
+  def test_initialize_power
+    @action = Action.new("The Ardent Adept", :Character, :Power, "Execute Perform text on a card", [:Perform])
+    @action = ActionSet.get_by_name("The Ardent Adept")[0]
+    assert_equal(:Character, @action.card_type)
+    assert_equal(:Power, @action.action_type)
+    assert_equal("The Ardent Adept", @action.name)
+    assert_equal("Execute Perform text on a card", @action.text)
+    assert_equal([:Perform], @action.power_invoke)
+    assert(@action.invokable?)
+  end
+  def test_to_s
+    assert_equal("Rhapsody of Vigor, Melody, Perform, Up to 5 targets regain 1 HP each", @action.to_s)
+  end
+end
+
+
+
+class TestActionChainGenerator < Test::Unit::TestCase
+  
+  def test_empty_action_list
+    actionChainGenerator = ActionChainGenerator.new([])
+    actionChainGenerator.generate_chains
+    assert_equal(0, actionChainGenerator.chains.length)
+    assert_equal("No Chains Found", actionChainGenerator.to_s)
+  end
+  
+  def test_one_power_list
+    actionChainGenerator = ActionChainGenerator.new([ ActionSet.get_by_name("The Ardent Adept")[0] ])
+    actionChainGenerator.generate_chains
+    assert_equal(1, actionChainGenerator.chains.length)
+    assert_equal(1, actionChainGenerator.chains[0].length)
+    assert_equal("Chain 1:\n  The Ardent Adept, Character, Power, Execute Perform text on a card\n", actionChainGenerator.to_s)
+  end
+  
+  def test_char_power_to_single_perform
+    action_list = ActionSet.get_by_name("Rhapsody of Vigor") +  ActionSet.get_by_name("The Ardent Adept")
+    actionChainGenerator = ActionChainGenerator.new(action_list)
+    actionChainGenerator.generate_chains
+    assert_equal(1, actionChainGenerator.chains.length)
+    assert_equal(2, actionChainGenerator.chains[0].length)
+    assert_equal("Chain 1:\n" + 
+                 "  The Ardent Adept, Character, Power, Execute Perform text on a card\n" +
+                 "  Rhapsody of Vigor, Melody, Perform, Up to 5 targets regain 1 HP each\n", actionChainGenerator.to_s)
+
+  end
+  
+  def test_char_power_to_double_perform
+    action_list = ActionSet.get_by_name("Rhapsody of Vigor") + ActionSet.get_by_name("The Ardent Adept") + ActionSet.get_by_name("Sarabande of Destruction")
+    actionChainGenerator = ActionChainGenerator.new(action_list)
+    actionChainGenerator.generate_chains
+    assert_equal(2, actionChainGenerator.chains.length, "number of chains should be 2")
+    assert_equal(2, actionChainGenerator.chains[0].length, "first chain length should be 2")
+    assert_equal(2, actionChainGenerator.chains[1].length, "second chain length should be 2")
+    string_val = "Chain 1:\n" + 
+                 "  The Ardent Adept, Character, Power, Execute Perform text on a card\n" +
+                 "  Rhapsody of Vigor, Melody, Perform, Up to 5 targets regain 1 HP each\n" +
+                 "Chain 2:\n" + 
+                 "  The Ardent Adept, Character, Power, Execute Perform text on a card\n" +
+                 "  Sarabande of Destruction, Melody, Perform, Destroy 1 ongoing or environment card\n" 
+    assert_equal(string_val, actionChainGenerator.to_s)
+
+  end
+  
+  
+end
